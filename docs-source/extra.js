b8857bf37d15b25bb59b6af742e982179410e913
moori <info@moorleiche.com>
2022-10-08 20:55:52 +0200

    initial

--- docs-source/extra.js
+++ docs-source/extra.js
@@ -13,14 +13,17 @@ $('.md-content p > img').each(function () {
     $(this).wrap('<a data-fancybox="gallery" title="' + alt + '" href="' + url + '"></a>');
 });
 
-var _paq = window._paq = window._paq || [];
-_paq.push(["setDoNotTrack", true]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-    var u="//matomo.moori.net/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '3']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-})();
+var consent = __md_get("__consent")
+if (consent && consent.matomo) {
+    var _paq = window._paq = window._paq || [];
+    _paq.push(["setDoNotTrack", true]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//matomo.moori.net/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '3']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+}
